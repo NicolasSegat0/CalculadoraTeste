87183bcc5468281ab0e7dedf077e11a2f676d90e
Nicolas Navas Segato <100158925+NicolasSegat0@users.noreply.github.com>
2023-10-29 16:02:45 -0300

    Update Calculadora.java
    Documentação da classe.

--- Calculadora/src/Calculadora/Calculadora.java
+++ Calculadora/src/Calculadora/Calculadora.java
@@ -1,24 +1,51 @@
 package Calculadora;
 
+/**
+ * @author Nicolas Navas Segato. 
+ * @version 1.05. 
+ * @since Release 1.02 da aplicação.
+ * 
+ * Uma classe simples que realiza adição, subtração, multiplicação e divisão.
+ */
+
 public class Calculadora {
 	
-	// Operação de adição 
+	/**
+	 * Método utilizado para calcular adição de dois números. 
+	 * @param a Primeiro número inserido. 
+	 * @param b Segundo número inserido. 
+	 * @return Retorna o resultado.
+	 */
 	public int soma(int a, int b) {
 		return a + b;
-	
 	}
 	
-	// Operação de subtração.
+	/**
+	 * Método utilizado para calcular subtração de dois números. 
+	 * @param a Primeiro número inserido. 
+	 * @param b Segundo número inserido. 
+	 * @return Retorna o resultado.
+	 */
 	public int subt(int a, int b) {
 		return a - b; 
 	}
 	
-	//Operação de multiplicação.
+	/**
+	 * Método utilizado para calcular multiplicação de dois números. 
+	 * @param a Primeiro número inserido. 
+	 * @param b Segundo número inserido. 
+	 * @return Retorna o resultado.
+	 */
 	public int multi(int a, int b) {
 		return a * b; 
 	}
 	
-	//Operação de divisao com tratamento de erro ao dividir um número por zero.
+	/**
+	 * Método utilizado para calcular divisão de dois números. 
+	 * @param a Primeiro número inserido. 
+	 * @param b Segundo número inserido. 
+	 * @return Retorna o resultado.
+	 */
 	public double divisao(int a, int b) {
 		if (b == 0) {
 			throw new ArithmeticException("Não é possíevl efetuar a operação");
